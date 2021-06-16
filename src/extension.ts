6ff3c2ab55fd126da8c9273b2caa80c8479586c4
George Semenov <george.sem@live.ru>
2021-06-16 23:37:38 +0000

    Update extension.ts - hot fix 2

--- src/extension.ts
+++ src/extension.ts
@@ -1,305 +1,200 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { Client } from '../clients/client';
-import { Gists } from '../clients/gists';
-import { Pastebin } from '../clients/pastebin';
-import { UtilClasses } from "../clients/util";
-import { Storage } from "../storage/storage";
-
+import { UtilClasses } from './clients/util';
+import { Client } from './clients/client';
+import { Pastebin } from './clients/pastebin';
+import { Gists } from './clients/gists';
+import { Storage } from './storage/storage';
+import { Gistify } from './gistify/gistify';
+import { NodeReferencesProvider, ReferenceTreeItem } from './gistify/referencesView';
+import { getVSCodeDownloadUrl } from 'vscode-test/out/util';
 let opn = require('opn');
 
-export module Gistify {
 
 
-    export class FileInfo {
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
 
 
+	// vscode.window.createTreeView('nodeReferences', {
+	// 	treeDataProvider: new NodeReferencesProvider()
+	// });
 
-    };
+	const nodeDependenciesProvider = new NodeReferencesProvider();
+	vscode.window.registerTreeDataProvider('nodeReferences', nodeDependenciesProvider);
 
-    export class References {
+	let refreshReferenceTable = () => nodeDependenciesProvider.refresh();
 
-        public static getPastebin(document : vscode.TextDocument) : Array<UtilClasses.Reference> {
-            var r = Array<UtilClasses.Reference>();
-            new Storage('pastebin').getReferences(document).forEach((value, index, array) => {
-                r.push(value);
-            });
-            return r;
-        }
+	vscode.commands.registerCommand('gistify.service.refreshReferenceTable', refreshReferenceTable);
 
-        public static getGists(document : vscode.TextDocument) : Array<UtilClasses.Reference> {
-            var r = Array<UtilClasses.Reference>();
-            new Storage('gists').getReferences(document).forEach((value, index, array) => {
-                r.push(value);
-            });
-            return r;
-        }
+	vscode.window.onDidChangeActiveTextEditor(() => {
+		vscode.commands.executeCommand('gistify.service.refreshReferenceTable');
+	});
 
-    }
-
-    export class Publish {
-
-        public static toPastebin(document: vscode.TextDocument, preferSelection: boolean, anonymously : boolean, privacy : Pastebin.Privacy, expire : Pastebin.ExpireDate) {
-            
-            new Promise((undefined, reject) => {
-
-                let snippet : UtilClasses.Snippet | null;
-                if (preferSelection) {
-                    snippet = UtilClasses.snippetFromCurrentSelection(document);
-                } else {
-                    snippet = UtilClasses.snippetFromCurrentFile(document);
-                }
-
-                if (snippet === null) {
-                    throw new Error("Snippet could not be created.");
-                }
-
-                var clientPromise: Promise<Pastebin.Client>;
-                if (anonymously) {
-                    clientPromise = Promise.resolve(Client.getAnonymousPastebinClient());
-                } else {
-                    clientPromise = Client.getUserPastebinClient();
-                }
-
-                clientPromise.then((client : Pastebin.Client) => {
-
-                    if (anonymously && privacy === Pastebin.Privacy.private) {
-                        privacy = Pastebin.Privacy.public;
-                    }
-
-                    client.upload(snippet!, privacy, expire).then((reference : Pastebin.Reference) => {
-                        vscode.window.showInformationMessage("File/selection has been published at " + reference.getLink(), ...["Open link..."]).then((value : string | undefined) => {
-                            if (value === "Open link...") {
-                                opn(reference.getLink());
-                            }
-                        });
-                        if (snippet!.getIsFile() && !document.isUntitled) {
-                            new Storage('pastebin').addReference(document, reference);
-                        } else {
-                            vscode.window.showWarningMessage("Publihed snippet is not tracked.\nUnknown, unworkspaced files and selections can not be tracked.", "OK");
-                        }
-                    }).catch((e : Error) => {
-                        vscode.window.showErrorMessage(e.toString());
-                    });
-
-                }).catch((e : Error) => {
-                    vscode.window.showErrorMessage(e.toString());
-                });
-
-            }).catch((e : Error) => {
-                vscode.window.showErrorMessage(e.toString());
-            });
-
-        }
+	setInterval(refreshReferenceTable, 60 * 1000);
 
 
-        public static toGists(document: vscode.TextDocument, preferSelection: boolean, privacy : Gists.Privacy, description : string) {
-            
-            new Promise((undefined, reject) => {
-
-                let snippet : UtilClasses.Snippet | null;
-                if (preferSelection) {
-                    snippet = UtilClasses.snippetFromCurrentSelection(document);
-                } else {
-                    snippet = UtilClasses.snippetFromCurrentFile(document);
-                }
-
-                if (snippet === null) {
-                    throw new Error("Snippet could not be created.");
-                }
-
-                Client.getGistsClient().then((client : Gists.Client) => {
-
-                    client.upload(snippet!, privacy, description).then((reference : Gists.Reference) => {
-                        vscode.window.showInformationMessage("File/selection has been published at " + reference.getLink(), ...["Open link..."]).then((value : string | undefined) => {
-                            if (value === "Open link...") {
-                                opn(reference.getLink());
-                            }
-                        });
-                        if (snippet!.getIsFile() && !document.isUntitled) {
-                            new Storage('gists').addReference(document, reference);
-                        } else {
-                            vscode.window.showWarningMessage("Publihed snippet is not tracked.\nUnknown, unworkspaced files and selections can not be tracked.", "OK");
-                        }
-                    }).catch((e : Error) => {
-                        vscode.window.showErrorMessage(e.toString());
-                    });
-
-                }).catch((e : Error) => {
-                    vscode.window.showErrorMessage(e.toString());
-                });
-
-            }).catch((e : Error) => {
-                vscode.window.showErrorMessage(e.toString());
-            });
-
-        }
+	  
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
 
 
-    }
+	let publishPastebinAnonymousCommand = vscode.commands.registerCommand('gistify.publish.pastebin.anonymous', () => {
+
+		try {
+			
+			let d = vscode.window.activeTextEditor?.document;
+			if ((d === undefined) || (d.getText() === '')) {
+				throw new Error("No current document.");
+			}
+
+			Gistify.selectSource().then((fromFile: boolean) => {
+				Gistify.publishPastebin(d!, fromFile, true).then(() => {
+
+				});
+			});
+		} catch (e : any) {
+			vscode.window.showErrorMessage(e.toString());
+		}
+
+	});
+
+	let publishPastebinCommand = vscode.commands.registerCommand('gistify.publish.pastebin', () => {
+
+		try {
+			
+			let d = vscode.window.activeTextEditor?.document;
+			if ((d === undefined) || (d.getText() === '')) {
+				throw new Error("No current document.");
+			}
+
+			Gistify.selectSource().then((fromFile: boolean) => {
+				Gistify.publishPastebin(d!, fromFile, false).then(() => {
+
+				});
+			});
+		} catch (e : any) {
+			vscode.window.showErrorMessage(e.toString());
+		}
+
+	});
+
+	let publishGistsCommand = vscode.commands.registerCommand('gistify.publish.gists', () => {
+
+		try {
+			
+			let d = vscode.window.activeTextEditor?.document;
+			if ((d === undefined) || (d.getText() === '')) {
+				throw new Error("No current document.");
+			}
+
+			Gistify.selectSource().then((fromFile: boolean) => {
+					Gistify.publishGists(d!, fromFile).then(() => {
+
+					});
+			});
+
+		} catch (e : any) {
+			vscode.window.showErrorMessage(e.toString());
+		}
+
+	});
+
+	context.subscriptions.push(publishGistsCommand, publishPastebinCommand, publishPastebinAnonymousCommand);
+
+	let publishCommand = vscode.commands.registerCommand('gistify.publish', () => {
+
+		try {
+			
+			let d = vscode.window.activeTextEditor?.document;
+			if ((d === undefined) || (d.getText() === '')) {
+				throw new Error("No current document.");
+			}
+
+			Gistify.selectSource().then((fromFile: boolean) => {
+
+				Gistify.selectService().then((i : number) => {
+
+					if ((i === 0) || (i === 1)) {
+						Gistify.publishPastebin(d!, fromFile, i === 0).then(() => {
+
+						});
+					}
+
+					if (i === 2) {
+						Gistify.publishGists(d!, fromFile).then(() => {
+
+						});
+					}
+
+				});
+			});
+		} catch (e : any) {
+			vscode.window.showErrorMessage(e.toString());
+		}
 
 
-
-    interface IterableQuickPick extends vscode.QuickPickItem {
-        i: number
-    }
-    
-    export function selectService() : Thenable<number> {
-        let services : Array<IterableQuickPick> = [
-            {
-                label: "Pastebin (anonymous)",
-                description: "",
-                i: 0
-            }, {
-                label: "Pastebin",
-                description: "",
-                i: 1
-            }, {
-                label: "Gists",
-                description: "",
-                i: 2
-            }
-        ];
-        return new Promise<number>((accept, reject) => {
-            vscode.window.showQuickPick(services, {title: "Select service to publish:"}).then((selection: IterableQuickPick | undefined) => {
-                if (selection !== undefined) {
-                    accept(selection.i);
-                } else{
-                    reject(-1);
-                }
-            });
-        });
-    }
-    
-    export function selectSource() : Thenable<boolean> {
-        let fileOrSelection : Array<IterableQuickPick> = [
-            {
-                label: "Publish whole file",
-                description: "Publish current opened file", // + UtilClasses.snippetFromCurrentFile(d)?.getName(),
-                i: 0
-            }, {
-                label: "Publish selection",
-                description: "Snippet from current document selection",
-                i: 1
-            }
-        ];
-        return new Promise<boolean>((accept, reject) => {
-            var selectionPromise = vscode.window.showQuickPick(fileOrSelection, {title: "Select snippet source:"});
-            if (UtilClasses.isSelectionEmpty()) {
-                selectionPromise = Promise.resolve(fileOrSelection[0]);
-            }
-            selectionPromise.then((selection: IterableQuickPick | undefined) => {
-                if (selection !== undefined) {
-                    accept(selection.i === 0);
-                } else {
-                    reject(true);
-                }
-            });
-        });
-    }
-    
-    export function publishPastebin(d : vscode.TextDocument, wholeFile : boolean, anonymous : boolean) : Thenable<void> {
-           
-        if (d.getText() === "") {
-            vscode.window.showErrorMessage('Empty document.');
-            return Promise.resolve();
-        }
-
-        let pasteBinPrivacy : Array<IterableQuickPick> = [
-            {
-                label: "Public",
-                description: "",
-                i: 0
-            }, {
-                label: "Unlisted",
-                description: "",
-                i: 1
-            }
-        ];
-    
-        let pasteBinExpire : Array<vscode.QuickPickItem> = [
-            { label: "N",description: "Will be kept forever."},
-            { label: "10M",description: "10 minutes"},
-            { label: "1H",description: "1 hour"},
-            { label: "1D",description: "1 day"},
-            { label: "1W",description: "1 week"},
-            { label: "2W",description: "2 weeks"},
-            { label: "1M",description: "1 month"},
-            { label: "6M",description: "6 months"},
-            { label: "1Y",description: "1 year"},
-        ];
-    
-        var privacy = -1;
-    
-        if (!anonymous) {
-            pasteBinPrivacy.push({
-                label: "Private",
-                description: "For user " + vscode.workspace.getConfiguration("gistify.pastebin").get('defaultUserName'),
-                i: 2
-            });
-        }
-    
-        return vscode.window.showQuickPick(pasteBinPrivacy, {title: "Specify Pastebin privacy:"}).then((selection: IterableQuickPick | undefined) => {
-            if (selection !== undefined) {
-                privacy = selection.i;
-            } else {
-                return Promise.resolve();
-            }
-        }).then(() => {
-            if (privacy === -1) {
-                return Promise.resolve();
-            }
-            vscode.window.showQuickPick(pasteBinExpire, {title: "Specify Pastebin expire date:"}).then((selection: vscode.QuickPickItem | undefined) => {
-                if (selection !== undefined) {
-                    var exp = selection.label as Pastebin.ExpireDate;
-                    Gistify.Publish.toPastebin(d!, wholeFile, anonymous, privacy, exp);
-                }
-            });
-        });
-    
-    }
-    
-    
-    export function publishGists(d : vscode.TextDocument, wholeFile : boolean) : Thenable<void> {
-    
-        if (d.getText() === "") {
-            vscode.window.showErrorMessage('Empty document.');
-            return Promise.resolve();
-        }
-
-        let gistsPrivacy : Array<IterableQuickPick> = [
-            {
-                label: "Public",
-                description: "",
-                i: 0
-            }, {
-                label: "Private",
-                description: "",
-                i: 1
-            }
-        ];
-    
-        var privacy = -1;
-    
-        return vscode.window.showQuickPick(gistsPrivacy, {title: "Specify Gists privacy:"}).then((selection: IterableQuickPick | undefined) => {
-            if (selection !== undefined) {
-                privacy = selection.i;
-            } else {
-                return Promise.resolve();
-            }
-        }).then(() => {
-            if (privacy === -1) {
-                return Promise.resolve();
-            }
-            vscode.window.showInputBox({title: "Optionally: specify description for this gist."}).then((description: string | undefined) => {
-                if (description !== undefined) {
-                    Gistify.Publish.toGists(d!, wholeFile, (privacy === 0 ? Gists.Privacy.public : Gists.Privacy.private), description!);
-                }
-            });
-        });
-    
-    }
-    
+	});
 
 
+	let publishSelectionCommand = vscode.commands.registerCommand('gistify.publishSelection', () => {
+
+		try {
+			
+			let d = vscode.window.activeTextEditor?.document;
+			if ((d === undefined) || (d.getText() === '')) {
+				throw new Error("No current document.");
+			}
+
+			let fromFile: boolean = false;
+
+			Gistify.selectService().then((i : number) => {
+
+				if ((i === 0) || (i === 1)) {
+					Gistify.publishPastebin(d!, fromFile, i === 0).then(() => {
+
+					});
+				}
+
+				if (i === 2) {
+					Gistify.publishGists(d!, fromFile).then(() => {
+
+					});
+				}
+
+			});
+
+		} catch (e : any) {
+			vscode.window.showErrorMessage(e.toString());
+		}
+
+	});
+
+
+	context.subscriptions.push(publishCommand, publishSelectionCommand);
+
+	let openReferenceTreeItemCommand = vscode.commands.registerCommand(
+		'gistify.service.openReferenceTreeItem',
+		(item: ReferenceTreeItem) => opn(item.getLink())
+	);
+
+	context.subscriptions.push(openReferenceTreeItemCommand);
+
+	let copyReferenceTreeItemLink = vscode.commands.registerCommand(
+		'gistify.service.copyReferenceTreeItemLink',
+		(item: ReferenceTreeItem) => {
+			vscode.env.clipboard.writeText(item.getLink());
+			vscode.window.showInformationMessage("Link copied to clipboard.");
+		}
+	);
+
+	context.subscriptions.push(copyReferenceTreeItemLink);
 
 }
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
+
