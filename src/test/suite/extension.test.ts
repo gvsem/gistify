a5a33649cc47f195f6befc5ac4ef36958ea45300
Ilya Lunev <ilunev@protonmail.com>
2021-06-17 06:45:08 +0300

    Добавил тест на Snippet

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,40 @@
 import * as assert from 'assert';
+import * as tmp from 'tmp';
+import * as path from 'path';
+import * as fs from 'fs';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+import { UtilClasses } from '../../clients/util';
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	test('Snippet handles file', async () => {
+
+		tmp.dir((err, dirPath, cleanupCallback) => {
+			if (err) {
+				throw err;
+			}
+
+			const filePath = path.join(dirPath, "code.py");
+
+			fs.writeFile(dirPath, 'print("Hello")', async () => {
+
+				const uri = vscode.Uri.file(filePath);
+				const document = await vscode.workspace.openTextDocument(uri);
+				const snippet = UtilClasses.snippetFromCurrentFile(document)!;
+
+				assert.strictEqual('python', snippet.getFormat());
+				assert.strictEqual('code.py', snippet.getName());
+				assert.strictEqual(filePath, snippet.getFilename());
+				assert.strictEqual('print("Hello")', snippet.getData());
+				assert.ok(snippet.getIsFile());
+
+			});
+
+			cleanupCallback();
+		});
 	});
 });
